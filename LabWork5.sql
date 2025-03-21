945201aa25d7d96919849a40aac768e0fec4146d
amadesya <154593967+amadesya@users.noreply.github.com>
2025-03-21 22:53:28 +0300

    Update LabWork5.sql

--- LabWork5.sql
+++ LabWork5.sql
@@ -1,1 +1,18 @@
+CREATE TABLE `market`.`tempbooks` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `surname` VARCHAR(50) NOT NULL,
+  `name` VARCHAR(50) NOT NULL,
+  `titlle` VARCHAR(50) NOT NULL,
+  `price` DECIMAL(6,2) NOT NULL DEFAULT 0,
+  PRIMARY KEY (`id`));
 
+INSERT INTO `market`.`tempbooks` (surname, name, titlle, price)
+SELECT 
+    authors.surname,
+    authors.name,
+    book.title,
+    book.price
+FROM 
+    authors
+JOIN 
+    book ON authors.id = book.author_id;
