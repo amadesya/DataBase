30ad84b903c7f322ddda0abda9b0cafbdb4c1d4b
amadesya <154593967+amadesya@users.noreply.github.com>
2025-03-21 23:19:20 +0300

    Update LabWork5.sql

--- LabWork5.sql
+++ LabWork5.sql
@@ -16,3 +16,32 @@ FROM
     authors
 JOIN 
     book ON authors.id = book.author_id;
+
+DELETE FROM `market`.`tempbooks`
+WHERE `titlle` LIKE '%компьютер%';
+
+UPDATE `market`.`tempbooks`
+SET 
+    `price` = CASE 
+        WHEN `surname` = 'Пушкин' THEN `price` * 2
+        WHEN `surname` = 'Иванов' THEN `price` - 50
+        ELSE `price`
+    END
+WHERE `id` > 0;  -- Условие с индексированным столбцом
+
+TRUNCATE TABLE `market`.`tempbooks`;
+
+UPDATE book
+        JOIN
+    authors ON book.author_id = authors.id 
+SET 
+    book.price = book.price + 100
+WHERE
+    authors.country = 'Росcия'
+
+DELETE FROM `customers`
+WHERE `id` NOT IN (
+    SELECT `customers_id`
+    FROM `orders`
+);
+
